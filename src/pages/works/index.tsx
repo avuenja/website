e19d95bf0592c937d560374ded6e34e12c81ecc2
Marcelo Pecin <celopeccin@gmail.com>
2022-07-21 00:40:26 +0200

    feat: Added new work: Gerador de CPF, CNPJ e RG válidos

--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -1,8 +1,24 @@
+import { SimpleGrid } from '@chakra-ui/react'
+
 import type { NextPageWithLayout } from '@/types'
 import MainLayout from '@/components/layouts/main'
+import Section from '@/components/section'
+import GridItem from '@/components/grid-item'
 
 const Works: NextPageWithLayout = () => {
-  return <></>
+  return (
+    <>
+      <Section>
+        <SimpleGrid columns={{ base: 1, sm: 2 }} spacing="10">
+          <GridItem
+            title="Gerador de CPF, CNPF e RG válidos"
+            thumbnail="/works/gerador-cpf-cnpj.jpeg"
+            href="https://gerador.progmar.dev"
+          />
+        </SimpleGrid>
+      </Section>
+    </>
+  )
 }
 
 Works.getLayout = (page: React.ReactElement) => <MainLayout>{page}</MainLayout>
